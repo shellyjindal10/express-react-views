4ab8ae9e1e1a25211b0c997347787153ba63eb31
Shelly Jindal <sjindal@C02PHGACG3QC.local>
2017-01-11 02:39:17 -0800

    first commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var logger = require('morgan');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+var reactViews = require('express-react-views');
+
+var app = express();
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', reactViews.createEngine());
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', routes.index);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
